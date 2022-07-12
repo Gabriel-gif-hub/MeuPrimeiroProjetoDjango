f4619f68c8444c40b6f3e760218d3bb6afe4128c
gabriel-gif-hub <gabrieloliveira2505@gmail.com>
2022-07-11 21:55:08 -0300

    Melhorias na lista de funcionarios

--- Rh/models.py
+++ Rh/models.py
@@ -18,13 +18,15 @@ class Funcionario(models.Model):
         ('GR','Gerente') 
     ]
 
-
     matricula = models.IntegerField()
     nome = models.CharField(max_length=50)
     cargo = models.CharField(max_length=2, choices= CARGOS)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     salario = models.DecimalField(max_digits=10,decimal_places=2)
     data_de_nascimento = models.DateField(null=True)
+
+class Meta:
+    ordering = ['nome']
 
 
     def __str__(self):
